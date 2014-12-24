25312366799955dd2146c4cefc29a22496999bba
Pavitheran Subenderan <psubende@uwaterloo.ca>
2014-12-24 00:55:22 -0500

    Created function which searches timetable dictionary by day and time.

--- ttable_parser.py
+++ ttable_parser.py
@@ -29,7 +29,7 @@ def linkparse(link, cdict):
     shorten = soup.find_all('tr')[3:]
     classlist = []
     for row in shorten:
-        i = [];
+        i = []
         for cell in row:
             if cell.string != None and cell.string.strip() != '' and cell.string.strip() != '':
                 i.append(cell.string)
@@ -43,11 +43,8 @@ def linkparse(link, cdict):
         for item in list:
             if checkdate(item):
                 appendDict(cdict, item, activec)
-<<<<<<< HEAD
 
-=======
->>>>>>> 5ea316a430f865ae4193ecdaeb4812d8ed339236
     return cdict
 
 
-#linkparse("http://www.artsandscience.utoronto.ca/ofr/timetable/winter/csb.html", {})
+#print(linkparse("http://www.artsandscience.utoronto.ca/ofr/timetable/winter/csb.html", {}))
