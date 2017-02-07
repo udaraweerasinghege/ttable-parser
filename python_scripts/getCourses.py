fa28cc673de7db9920cc8eba5e1f759a3d927e5f
Udara Weerasinghege <udara.weerasinghege@mail.utoronto.ca>
2017-02-06 19:47:42 -0500

    add utsg back

---
+++ python_scripts/getCourses.py
@@ -0,0 +1,37 @@
+import requests
+course_dir = []
+course_link_tmpl = 'http://calendar.artsci.utoronto.ca/crs_{0}.htm#{1}'
+# get all courses from 1st year to 4th
+for year in range(1,5):
+    resp = requests.get('https://timetable.iit.artsci.utoronto.ca/api/courses?org=&code=&section=&studyyear={}&daytime=&weekday=&prof=&breadth=&online=&waitlist=&available=&title='.format(year))
+    data = resp.json()
+    for course,course_data in data.items():
+        course_code = course_data['code'] + course_data['section']
+        course_link = course_link_tmpl.format(course_code[0:3].lower(), course_code[0:8])
+        breadth = course_data['breadthCategories']
+        meeting_sections = []
+
+        for meetings,lecture in course_data['meetings'].items():
+            try:
+                for schedule, session in lecture['schedule'].items():
+                    if (session['meetingDay'] and session['meetingStartTime']):
+                        meeting_sections.append({
+                            'day': session['meetingDay'],
+                            'start': session['meetingStartTime']
+                        })
+            except Exception as e:
+                # some lecture sessions' schedule is set to an empty array for some reason instead of object
+                # way to have a standardized data structure uoft >.<
+                print(e)
+                continue
+        course_dir.append({
+            'code': course_code,
+            'link': course_link,
+            'breadth_req': breadth,
+            'meeting_sections': meeting_sections,
+        })
+
+print((course_dir))
+
+
+
--- utsc/js/dictsearch.js
+++ utsc/js/dictsearch.js
@@ -1,4 +1,19 @@
-{
+// JavaScript Document
+var day;
+var str = "";
+var val;
+var sem;
+var year = '0';
+var req = '0';
+
+var year_index = {'1':'A', '2':'B', '3':'C', '4':'D'};
+var breadth_index = {'1':'Arts, Literature, and Language', '2':'History, Philosophy, and Cultural Studies', '3':'Social and Behavioural Sciences', '4':'Natural Sciences', '5':'Quantitative Reasoning'};
+var day_index = {'M':'MO', 'T':'TU', 'W':'WE', 'R':'TH', 'F':'FR'};
+var time_index = {'8':'8:00', '9':'9:00', '10':'10:00', '11':'11:00', '12':'12:00', '1':'13:00', '2':'14:00', '3':'15:00', '4':'16:00', '5':'17:00', '6':'18:00', '7':'19:00'};
+
+
+//-------------------------------------------------------------------------------------------------------------------------
+var course_list = {
 	'UTSC_COURSES': [{
 		'code': 'ACMB01H3F',
 		'link': 'http://www.utsc.utoronto.ca/~registrar/calendars/calendar/Arts,_Culture_and_Media.html#ACMB01H3',
@@ -12290,3 +12305,128 @@
 		}]
 	}]
 }
+
+//-------------------------------------------------------------------------------------------------------------------------
+$(document).ready(function(){
+
+
+	$( "#time" )
+	.change(function () {
+		
+		$( "#time option:selected" ).each(function() {
+                        val = $(this).val();
+			str = dictSearch(day,val,sem,year,req) ;
+			});
+			$( "pre#output" ).html( str );
+			})
+			.change();
+
+	$( "#breadth" )
+	.change(function () {
+		
+		$( "#breadth option:selected" ).each(function() {
+                        req = $(this).val();
+			str = dictSearch(day,val,sem,year,req) ;
+			});
+			$( "pre#output" ).html( str );
+			})
+			.change();
+	
+	$(function () { 
+    $("#tip").popover({
+        //title: 'Enter Mobile Number', 
+        content: "Duplicate course listings means multiple sections for queried times.",
+        trigger: 'hover',
+        delay: {show: 0, hide: 0}
+    });  
+});
+});
+
+ 
+	$('body').on('click', '#stubb.btn-group button', function (e) {
+		
+		day = $(this).val();
+		str = dictSearch(day, val, sem, year, req);
+		$("pre#output").html(str);
+		
+	
+	
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+    //do any other button related things
+});
+
+$('body').on('click', '#sem.btn-group button', function (e) {
+		
+		sem = $(this).val();
+		str = dictSearch(day, val, sem, year, req);
+		$("pre#output").html(str);
+		
+	
+	
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+	
+    //do any other button related things
+});
+
+$('body').on('click', '#years.btn-group button', function (e) {
+		
+		year = $(this).val();
+		str = dictSearch(day, val, sem, year, req);
+		$("pre#output").html(str);
+		
+	
+	
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+    //do any other button related things
+});
+
+function make_course_link(course){
+    return '<a href="' + course.link + '" target="_blank">' + course.code + '</a>';
+}
+
+// Convert array into array of arrays with each inner array containing five items.
+function fives(lst) {
+	output = [];
+	for (var i = 0; i<lst.length; i++){
+		var courseLink = lst[i];
+		if (i % 5 == 0 && i != 0) {
+			output.push('\n' + "  " + courseLink);
+		}
+		else {
+			output.push("  " + courseLink);
+		}
+	}
+	return output;
+}
+function dictSearch(day, time, sem, year, breadth) {
+    var result = [];
+    var myArray = course_list.UTSC_COURSES;
+	for (var i = 0; i < myArray.length; i++){
+	    course = myArray[i];
+	    //Make sure the semester matches
+	    if (course.code.slice(-1) == sem){
+	        //Make sure the year matches
+		    if (course.code.charAt(3) == year_index[year] || year == '0') {
+		        //Make sure the breadth matches
+			    if (course.breadth_req == breadth_index[breadth] || breadth === '0') {
+			        //Make sure day matches
+				    for(var j = 0; j < course.meeting_sections.length; j++){
+				        if (course.meeting_sections[j].day == day_index[day]){	
+						    //Finally, make sure start time matches
+			                if (time == '*' || course.meeting_sections[j].start == time_index[time]){
+							    result.push(make_course_link(course));
+							}					  
+				        }
+			        }
+		        }
+	        }
+	    }
+	}
+	return fives(result);
+}
+
+
+	
